b9783f3933923cae04c7868b05df90035074ec2d
lajtosdavid11 <lajtos.david11@gmail.com>
2020-09-24 08:22:52 +0200

    Add project files.

---
+++ Raktar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Raktar
+{
+    class Program
+    {
+
+        static List<Termek> termekek = new List<Termek>();
+        static void beolvas_raktar()
+        {
+            StreamReader sr = new StreamReader("raktar.csv");
+            
+            while (!sr.EndOfStream) 
+            {
+                /*
+                 sor[0] -> kod
+                 sor[1] -> ár stb
+                 
+                 */
+                string[] sor = sr.ReadLine().Split(';');
+                Console.WriteLine(sor[0]);
+                
+                
+                termekek.Add(new Termek(sor[0], sor[1],int.Parse(sor[2]),int.Parse(sor[3])));
+                
+
+
+            }
+
+            sr.Close();
+
+
+            for (int i = 0; i < termekek.Count; i++)
+            {
+                Console.WriteLine(termekek[i].KOD);
+            }
+        }
+        static void Main(string[] args)
+        {
+            beolvas_raktar();
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Raktar/Termek.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raktar
+{
+    class Termek
+    {
+        private string kod;
+        public string KOD {
+
+
+            get { return kod; }
+            set { kod = value; }
+        }
+        private string nev;
+
+        public string Nev { get { return nev; } set { nev = value; } }
+
+        private int ar;
+
+
+
+        public int AR
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
+        private int db;
+
+        public int DB
+        {
+            get { return db; }
+            set { db = value; }
+        }
+
+
+
+        public Termek (string kod, string nev, int ar,int db )
+        {
+            this.kod = kod;
+            this.nev = nev;
+            this.ar = ar;
+            this.db = db;
+
+
+        }
+
+
+    }
+}
